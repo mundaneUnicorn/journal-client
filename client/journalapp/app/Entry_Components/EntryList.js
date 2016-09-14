07ae874671c647ea3766417ad97f9e59f636b22b
Chris Chiang <cchiang1992@gmail.com>
2016-09-14 15:46:24 -0700

    added Swipeout but makes the app crash;

--- client/journalapp/app/Entry_Components/EntryList.js
+++ client/journalapp/app/Entry_Components/EntryList.js
@@ -8,16 +8,29 @@ import {
 } from 'react-native';
 
 // VB: Refactored require to use import, for consistency
+// import Swipeout from 'react-native-swipeout';
+var Swipeout = require('react-native-swipeout');
 import Entry from './Entry';
 import styles from '../styles/EntryListStyles';
 
 var EntryList = ({entries}) => (
     <ListView style ={styles.container}
-       dataSource={entries}
-       renderRow={ (rowData) => {
-
-          return (<Entry id={ rowData.id } votes={ rowData.votes } text={ rowData.text } createdAt={ rowData.createdAt } location={ rowData.location }/>)
-       }}/>
+      dataSource={entries}
+      renderRow={ (rowData) => {
+        let swipeBtn = [{
+          text: 'Delete',
+          backgroundColor: 'red',
+          underlayColor: 'rgba(0, 0, 0, 1, 0.6)',
+          onPress: () => {
+            // this.deleteNote(rowData)
+          }
+        }]
+        return (
+          <Swipeout right={swipeBtn}>
+            <Entry id={ rowData.id } votes={ rowData.votes } text={ rowData.text } createdAt={ rowData.createdAt } location={ rowData.location }/>
+          </Swipeout>
+        )
+      }}/>
 )
 
 module.exports = EntryList;
