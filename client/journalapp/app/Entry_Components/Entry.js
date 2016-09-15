0417a4df370e5020c927619138bbfb849fd5be10
ZolayvarE <ZolayvarECU@gmail.com>
2016-09-14 19:54:04 -0700

    Fix functions broken by merge conflict

--- client/journalapp/app/Entry_Components/Entry.js
+++ client/journalapp/app/Entry_Components/Entry.js
@@ -28,36 +28,54 @@ export default class Entry extends Component {
   constructor(props) {
     super(props);
     this.props = props;
-    this.entryContext = this;
+
+    var entryContext = this;
     this.likePost = () => {
-    };
-  }
-  
-  likePost() {
-    AsyncStorage.getItem('@MySuperStore:token', (err, token) => {
-      fetch('http://localhost:3000/api/likes', {
-        method: 'POST',
-        headers: {
-          'content-type': 'application/json',
-          'x-access-token': token,
-        },
-        body: JSON.stringify({ 
-          user: props.user,
-          entryId: props.id, 
-        }),
-      }).then(function (response) {
-        var votesArray = this.entryContext.props.votes;
-        var userIndex = votesArray.indexOf(user);
-        if (userIndex === -1) {
-          votesArray.push(user);
-        } else {
-          votesArray.splice(userIndex, 1);
+      
+      var token;
+      var user;
+      var queryServer = () => {
+        fetch('http://localhost:3000/api/likes', {
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json',
+            'x-access-token': token,
+          },
+          body: JSON.stringify({ 
+            user: user,
+            entryId: props.id, 
+          }),
+        }).then(function (response) {
+          var votesArray = entryContext.props.votes;
+          var userIndex = votesArray.indexOf(user);
+          if (userIndex === -1) {
+            votesArray.push(user);
+          } else {
+            votesArray.splice(userIndex, 1);
+          }
+          entryContext.forceUpdate();
+        }).catch(function (error) {
+          console.log(error);
+        });
+      };
+
+      var queryCounter = 0;
+      AsyncStorage.getItem('@MySuperStore:token', (err, retrievedToken) => {
+        queryCounter++;
+        token = retrievedToken;
+        if (queryCounter >= 2) {
+          queryServer();
         }
-        this.entryContext.forceUpdate();
-      }).catch(function (error) {
-        console.log(error);
       });
-    });
+
+      AsyncStorage.getItem('@MySuperStore:username', (err, username) => {
+        queryCounter++;
+        user = username;
+        if (queryCounter >= 2) {
+          queryServer();
+        }
+      });
+    };
   }
 
   render() {
