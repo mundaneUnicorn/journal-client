cafeb88127fa9e1b9322c4ea59713603e8b92bf1
ZolayvarE <ZolayvarECU@gmail.com>
2016-09-15 14:45:41 -0700

    Refactor entry to be in a more conventional react format.

--- client/journalapp/app/Entry_Components/Entry.js
+++ client/journalapp/app/Entry_Components/Entry.js
@@ -28,59 +28,69 @@ export default class Entry extends Component {
   constructor(props) {
     super(props);
     this.props = props;
-    this.user;
-    this.token;
-    this.full = styles.showImage;
-    this.empty = styles.showImage;
-
-    var entryContext = this;
-    this.likePost = () => {
-      fetch('http://localhost:3000/api/likes', {
-        method: 'POST',
-        headers: {
-          'content-type': 'application/json',
-          'x-access-token': entryContext.token,
-        },
-        body: JSON.stringify({ 
-          user: entryContext.user,
-          entryId: props.id, 
-        }),
-      }).then(function (response) {
-        var votesArray = entryContext.props.votes;
-        var userIndex = votesArray.indexOf(entryContext.user);
-        if (userIndex === -1) {
-          votesArray.push(entryContext.user);
-          entryContext.full = styles.showImage;
-          entryContext.empty = styles.hideImage;
-        } else {
-          votesArray.splice(userIndex, 1);
-          entryContext.full = styles.hideImage;
-          entryContext.empty = styles.showImage;
-        }
-        entryContext.forceUpdate();
-      }).catch(function (error) {
-        console.log(error);
-      });
+    this.state = {
+      user: undefined,
+      token: undefined,
+      full: styles.hideImage,
+      empty: styles.showImage,
     };
+
+  }
+
+  likePost() {
+    var entryContext = this;
+    fetch('http://localhost:3000/api/likes', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'x-access-token': entryContext.state.token,
+      },
+      body: JSON.stringify({ 
+        user: entryContext.state.user,
+        entryId: entryContext.props.id, 
+      }),
+    }).then(function (response) {
+      var votesArray = entryContext.props.votes;
+      var userIndex = votesArray.indexOf(entryContext.state.user);
+
+      if (userIndex === -1) {
+        votesArray.push(entryContext.state.user);
+        entryContext.setState({ 
+          full: styles.showImage,
+          empty: styles.hideImage,
+        });
+      } else {
+        votesArray.splice(userIndex, 1);
+        entryContext.setState({ 
+          full: styles.hideImage,
+          empty: styles.showImage,
+        });
+      }
+      entryContext.forceUpdate();
+    }).catch(function (error) {
+      console.log(error);
+    });
   }
 
   componentWillMount() {
     var entryContext = this;
     AsyncStorage.getItem('@MySuperStore:token', (err, retrievedToken) => {
-      entryContext.token = retrievedToken;
+      entryContext.setState({ token: retrievedToken });
     });
 
     AsyncStorage.getItem('@MySuperStore:username', (err, username) => {
-      entryContext.user = username;
+      entryContext.setState({ user: username });
       var userIndex = entryContext.props.votes.indexOf(username);
       if (userIndex === -1) {
-        entryContext.full = styles.hideImage;
-        entryContext.empty = styles.showImage;
-        entryContext.forceUpdate();
+        entryContext.setState({ 
+          full: styles.hideImage,
+          empty: styles.showImage,
+        });
       } else {
-        entryContext.full = styles.showImage;
-        entryContext.empty = styles.hideImage;
-        entryContext.forceUpdate();
+        entryContext.setState({ 
+          full: styles.showImage,
+          empty: styles.hideImage,
+        });
       }
     });
   }
@@ -101,13 +111,13 @@ export default class Entry extends Component {
             <Text style={ styles.entryText }>
               { this.props.text }     
             </Text>
-            <TouchableHighlight style={ styles.ratingContainer } onPress={ this.likePost }>
+            <TouchableHighlight style={ styles.ratingContainer } onPress={ this.likePost.bind(this) }>
               <View style={ styles.ratingContainer }>
                 <Text style={ styles.rating }>
                   { this.props.votes.length }
                 </Text> 
-                <Image style={ this.empty } source={require('../images/empty_heart.png')}></Image>
-                <Image style={ this.full } source={require('../images/full_heart.png')}></Image>
+                <Image style={ this.state.empty } source={require('../images/empty_heart.png')}></Image>
+                <Image style={ this.state.full } source={require('../images/full_heart.png')}></Image>
               </View>
             </TouchableHighlight>
           </View>
