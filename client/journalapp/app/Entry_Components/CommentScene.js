4cdf474b1da21380f280fcc2d1ed6766e5e03477
Chris Chiang <cchiang1992@gmail.com>
2016-09-15 17:19:22 -0700

    styling up textinput

--- client/journalapp/app/Entry_Components/CommentScene.js
+++ client/journalapp/app/Entry_Components/CommentScene.js
@@ -15,14 +15,36 @@ export default class CommentScene extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      dynamicHeight: () => { return {height: Dimensions.get('window').height - 49 - 70}}
     };
+  }
+
+  moveUpForKeyboardShow(){
+    setTimeout( ()=> {
+      this.setState(
+        { dynamicHeight : () => { return { height: Dimensions.get('window').height * .45 }} }
+      );
+    }, 200); 
+  }
+
+  moveDownForKeyboardHide(){
+    this.setState(
+      { dynamicHeight : () => { return {height: Dimensions.get('window').height - 49 - 70}} }
+    );
   }
 
   render() {
     return (
-      <View style={{paddingTop:500}}>
-        <TextInput placeholder={ `Enter a comment...` } />
+      <View style={ styles.container }>
+        <TextInput 
+          keyboardType='default'
+          keyboardAppearance='light' 
+          multiline={ true }
+          maxLength={ 600 }
+          placeholder='Begin a comment right over here dude...'
+          style={ [this.state.dynamicHeight(), styles.bodyWidth, styles.fadedText] }
+          onFocus= { this.moveUpForKeyboardShow.bind(this) }
+          onBlur= { this.moveDownForKeyboardHide.bind(this) }/>
       </View>
     )
   }
--- client/journalapp/app/styles/CommentSceneStyles.js
+++ client/journalapp/app/styles/CommentSceneStyles.js
@@ -4,7 +4,16 @@ import {
 } from 'react-native';
 
 const styles = StyleSheet.create({
-  
+  container: {
+    backgroundColor: '#f0f8ff',
+    color: 'black',
+    marginTop: 40,
+    borderWidth: 1,
+  },
+  text: {
+    color: 'black',
+    height: 40
+  }
 })
 
 module.exports = styles;
